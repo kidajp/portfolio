f3863155e0820d3dd1d6487873e2e716810b421f
kidajp <0814shun@gmail.com>
2025-03-23 22:12:19 +0900

    スキルダイアログの実装

--- src/pages/_skill/Skill.tsx
+++ src/pages/_skill/Skill.tsx
@@ -1,17 +1,22 @@
 "use clinet";
 
-import { ComponentProps } from "react";
+import { PropsWithChildren } from "react";
+import { Dialog } from "@base-ui-components/react/dialog";
 import styles from "./Skill.module.css";
 
-export const Skill = ({
-  children,
-  ...props
-}: Omit<ComponentProps<"button">, "type">) => {
+export const Skill = ({ children }: PropsWithChildren) => {
   return (
     <li>
-      <button {...props} type="button" className={styles.skill}>
-        {children}
-      </button>
+      <Dialog.Root>
+        <Dialog.Trigger className={styles.skill}>{children}</Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Backdrop className={styles.backdrop} />
+          <Dialog.Popup className={styles.popup}>
+            <Dialog.Title>{children}</Dialog.Title>
+            <Dialog.Description>あああああ</Dialog.Description>
+          </Dialog.Popup>
+        </Dialog.Portal>
+      </Dialog.Root>
     </li>
   );
 };
