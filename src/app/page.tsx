363703d0aa73bab8931c222ee26d0f992ae25f3f
kidajp <0814shun@gmail.com>
2025-03-24 17:10:08 +0900

    mdxを使えるようにする

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { basePath } from "../../next.config";
-const BASE_PATH = basePath ? basePath : "";
 
 export default function Home() {
   return (
@@ -9,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src={`${BASE_PATH}/next.svg`}
+          src="/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -31,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src={`${BASE_PATH}/vercel.svg`}
+              src="/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -56,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${BASE_PATH}/file.svg`}
+            src="file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -70,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${BASE_PATH}/window.svg`}
+            src="window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -84,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src={`${BASE_PATH}/globe.svg`}
+            src="globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
