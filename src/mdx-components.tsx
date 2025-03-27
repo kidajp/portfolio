5721aef0015d7fc9050299a33f1e54c019a6401e
kidajp <0814shun@gmail.com>
2025-03-27 16:48:08 +0900

    pタグの装飾

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,7 +2,8 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h2: (props) => <h2 {...props} className="mb-2 block text-2xl font-bold" />,
+    h2: (props) => <h2 {...props} className="mb-3 block text-2xl font-bold" />,
+    p: (props) => <p {...props} className="mb-13" />,
     ...components,
   };
 }
